df85653e1ab7eb2a2bcf088b1a0d5f1c68734be7
Khoi Tran <knt36@drexel.edu>
2016-11-04 17:29:58 -0400

    work

--- www/js/shared/controllers.js
+++ www/js/shared/controllers.js
@@ -14,17 +14,53 @@ appControllers.controller('MapCtrl', function($scope, $state, $cordovaGeolocatio
  
     $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
  
-	var marker = new google.maps.Marker({
+	var yourLocation = new google.maps.Marker({
           position: latLng,
           map: $scope.map,
-          title: 'Hello World!',
+          title: 'Current Location',
 		  animation: google.maps.Animation.DROP
         });
 		
+	var request = {
+	location: latLng,
+	radius: '500',
+	type: 'store'
+	  };
+	
+    service = new google.maps.places.PlacesService($scope.map);
+    service.textSearch(request, callback, $scope.map);	
+	  
   }, function(error){
     console.log("Could not get location");
   });
 });
+
+function createMarkers(places, map) {
+  var bounds = new google.maps.LatLngBounds();
+  var placesList = document.getElementById('places');
+  for (var i = 0, place; place = places[i]; i++) {
+    var image = {
+	  map: $scope.map,
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(25, 25)
+    };
+  }
+}
+
+function callback(results, status, map) {
+	//console.log(results);
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      var place = results[i];
+      createMarkers(results[i], map);
+    }
+  }
+}
+
+
 
 //This is Controller for Dialog box.
 appControllers.controller('DialogController', function ($scope, $mdDialog, displayOption) {
