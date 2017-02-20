de741e58ddb9d06550850a8f698ced8a0a0893f9
Justice Ogbonna <ebesco@yahoo.com>
2017-02-19 21:12:58 -0500

    added url links for all pages

--- v2/src/app/app.module.ts
+++ v2/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import {NgModule, ErrorHandler} from "@angular/core";
-import {RouterModule, Routes} from "@angular/router";
-import {IonicApp, IonicModule, IonicErrorHandler} from "ionic-angular";
+import {IonicApp, IonicModule, IonicErrorHandler, DeepLinkConfig} from "ionic-angular";
 import {MyApp} from "./app.component";
 import {AboutPage} from "../pages/about/about";
 import {ContactPage} from "../pages/contacts/contact";
@@ -18,6 +17,26 @@ import {Profile} from "../pages/profile/profile";
 import {Settings} from "../pages/settings/settings";
 import {Vitals} from "../pages/vitals-monitor/vitals";
 //import { Auth } from '../pages/auth/auth.module';
+
+const deepLinkConfig: DeepLinkConfig = <DeepLinkConfig>{
+    links: [
+        {component: ContactPage, name: "contact", segment: "contact"},
+        {component: AboutPage, name: "about", segment: "about"},
+        {component: HomePage, name: "home", segment: "home"},
+        {component: LoginPage, name: "login", segment: "login"},
+        {component: StartPage, name: "start", segment: "start"},
+        {component: SignUpPage, name: "signup", segment: "signup"},
+        {component: ResetPage, name: "reset", segment: "reset"},
+        {component: Devices, name: "devices", segment: "devices"},
+        {component: Emergency, name: "emergency", segment: "emergency"},
+        {component: Help, name: "help", segment: "help"},
+        {component: Carriers, name: "carriers", segment: "carriers"},
+        {component: Profile, name: "profile", segment: "profile"},
+        {component: Settings, name: "settings", segment: "settings"},
+        {component: Vitals, name: "vitals", segment: "vitals"},
+        {component: Dashboard, name: "dashboard", segment: "dashboard"}
+    ]
+};
 
 @NgModule({
     declarations: [
@@ -40,7 +59,7 @@ import {Vitals} from "../pages/vitals-monitor/vitals";
     ],
     imports: [
         //Auth,
-        IonicModule.forRoot(MyApp)
+        IonicModule.forRoot(MyApp, {}, deepLinkConfig)
     ],
     bootstrap: [IonicApp],
     entryComponents: [
