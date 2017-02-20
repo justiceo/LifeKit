4d8ed8c5fdc53588ae86cec68e98161f9967bc1d
Justice Ogbonna <ebesco@yahoo.com>
2017-02-19 19:47:35 -0500

    update angular to latest to support router

--- v2/src/app/app.module.ts
+++ v2/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import {NgModule, ErrorHandler} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
 import {IonicApp, IonicModule, IonicErrorHandler} from "ionic-angular";
 import {MyApp} from "./app.component";
 import {AboutPage} from "../pages/about/about";
