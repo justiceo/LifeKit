79f1dcbe0b47eaf489753b9a1141a82f8106243f
Justice Ogbonna <ebesco@yahoo.com>
2017-03-07 18:17:17 -0500

    add emergency controller

--- v2/src/pages/emergency/emergency.ts
+++ v2/src/pages/emergency/emergency.ts
@@ -1,11 +1,34 @@
 import {Component} from "@angular/core";
+import {Flashlight, Vibration} from "ionic-native";
 
 @Component({
     templateUrl: 'emergency.html'
 })
 export class Emergency {
 
-    constructor() {
+    isEmergencyMode: boolean;
 
+    constructor(private flashlight: Flashlight, private vibration: Vibration) {
+        this.enableEmergencyMode();
+    }
+
+    enableEmergencyMode() {
+        // switch on flashlight
+        if(Flashlight.available() && !Flashlight.isSwitchedOn())
+            Flashlight.switchOn();
+
+        // enable vibration //todo: implement with observable
+        setTimeout(() => {
+            if(this.isEmergencyMode)
+                Vibration.vibrate(1000);
+        }, 1000);
+
+    }
+
+    disableEmergencyMode() {
+        if(Flashlight.available() && Flashlight.isSwitchedOn())
+            Flashlight.switchOff();
+
+        this.isEmergencyMode = false;
     }
 }
